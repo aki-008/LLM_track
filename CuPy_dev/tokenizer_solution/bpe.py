e9479fbed7218935ec5edb1c4f3ca913f1ab8bd9
aki-008 <shaswatsavage@gmail.com>
2026-08-13 23:03:07 +0530

    (Cupy/Dataloader): dataloader notebook done

--- CuPy_dev/tokenizer_solution/bpe.py
+++ CuPy_dev/tokenizer_solution/bpe.py
@@ -37,11 +37,12 @@ GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}
 GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
 
 GPT4_SPECIAL_TOKENS = {
-    "<|endoftext|>": 100257,
-    "<|fim_prefix|>": 100258,
-    "<|fim_middle|>": 100259,
-    "<|fim_suffix|>": 100260,
-    "<|endofprompt|>": 100276,
+    '<|endoftext|>': 100257,
+    '<|fim_prefix|>': 100258,
+    '<|fim_middle|>': 100259,
+    '<|fim_suffix|>': 100260,
+    '<|endofprompt|>': 100276,
+    '<|pad|>': 100277,
 }
 
 
